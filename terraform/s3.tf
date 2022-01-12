814dc85a5e8aaf1f459e4a79075d981640bd1f12
Alex Chan <a.chan@wellcome.org>
2022-01-12 15:25:22 +0000

    Add versioning to the S3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,4 +10,27 @@ resource "aws_s3_bucket" "born_digital_transfer" {
       storage_class = "STANDARD_IA"
     }
   }
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    id      = "Delete old version"
+    enabled = true
+
+    noncurrent_version_transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
 }
